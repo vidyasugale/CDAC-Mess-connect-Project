ed63b67dc853ce8ca8943e1b73f55d50603e5557
ChandanBile <chandanbile100@gmail.com>
2023-08-25 02:32:18 +0530

    Login and contact pages added

--- frontend/messfrontend/src/components/body/Loginpg.js
+++ frontend/messfrontend/src/components/body/Loginpg.js
@@ -1,10 +1,59 @@
+import React, { useState } from 'react';
+import Navbar from '../header/Navbar';
+import Button from '../header/Button';
 const Loginpg = () => {
-    return(
-        <>
-        <div>
-            Login Page
+
+
+  //   const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // console.log('Logging in with:', username, email, password);
+  };
+
+  const handleRegister = () => {
+    // console.log('Registering:', username, email, password);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="login-container ">
+
+        <div className="" style={{ position: "relative", height: "5rem" }}>
+          <div className="largeFont zIndBack registerHeadOut" style={{ left: "20%" }}>
+            Log-in
+          </div>
+          <div className="smallFont zIndFront registerHeadIn" style={{}}>
+            Log-in
+          </div>
         </div>
-        </>
-    )
-}
+
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <Button classname="btn btn-md mt-3 border" btnText="Login" onClick={handleLogin} />
+        <p>OR</p>
+        <Button classname="btn btn-md border" btnText="Register" onClick={handleRegister} />
+      </div>
+    </>
+  );
+};
+
 export default Loginpg;
