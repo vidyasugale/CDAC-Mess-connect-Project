2a0aab42bb4190866d7ec4c10422b0e5ad64870f
“ayushi” <ayushimalviya279@gmail.com>
2023-08-28 23:28:57 +0530

    customer nav about & contact

---
+++ frontend/messfrontend/src/components/customer/CustomerAboutUs.js
@@ -0,0 +1,17 @@
+import Navbar2 from "../header/Navbar2";
+import Footer from "../footer/Footer";
+import AboutCard from "../body/AboutCard";
+
+const CustomerAboutUs = () => {
+    return(
+        <>
+        <Navbar2 />
+        <br/>
+        <AboutCard />
+        <Footer />
+        </>
+    )
+
+}
+
+export default CustomerAboutUs
