0515eaa5c904c7d644bd936ae797c8265d3ea812
“ayushi” <ayushimalviya279@gmail.com>
2023-08-29 18:12:28 +0530

    nav2 editProfile MainHomepg

---
+++ frontend/messfrontend/src/components/customer/EditProfile.js
@@ -0,0 +1,172 @@
+import Navbar2 from "../header/Navbar2"
+import Footer from "../footer/Footer"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import axiosConfig from "../../configs/axiosConfig"
+import Button from "../header/Button"
+
+const EditProfile = () => {
+    const [editData,setEditData] = useState({});
+    const [editUserData, setEditUserData] = useState({
+        firstName: "",
+        lastName: "",
+        email:"",
+        phone: "",
+        address: ""
+    })
+    useEffect(() => {
+        const data = JSON.parse(sessionStorage.getItem("customerData"))
+        console.log(data)
+        const getUserData = async() =>{
+            await axiosConfig.get(`/user/getuser/${data.id}`)
+            .then( response =>{
+                console.log(response.data);
+                setEditData(response.data);
+                console.log(editData);
+                setEditUserData({
+                    ...editUserData,
+                    email: `${editData.email}`
+                })
+            }
+
+            ).catch(e => console.log(e))
+
+            
+        }
+        getUserData();
+    },[])
+    const [invalidFirstName, setInvalidFirstName] = useState(false);
+    const [invalidLastName, setInvalidLastName] = useState(false);
+    // const [invalidPass, setInvalidPass] = useState(false);
+    const [invalidAddress, setInvalidAddress] = useState(false);
+    const [invalidPhone, setInvalidPhone] = useState(false);
+    // const [invalidRole, setInvalidRole] = useState(false);
+    const navigate = useNavigate();
+
+    const validateFirstName = (e) => {
+        if (/^[a-zA-Z]{3,30}$/.test(e.target.value)) {
+            setInvalidFirstName(false);
+            changeHandler(e);
+        } else {
+            setInvalidFirstName(true);
+        }
+    }
+
+    const validateLastName = (e) => {
+        if (/^[a-zA-Z]{3,30}$/.test(e.target.value)) {
+            setInvalidLastName(false);
+            changeHandler(e);
+        }
+        else {
+            setInvalidLastName(true);
+        }
+    }
+
+    const validateAddress = (e) => {
+        if (/^.{5,}$/.test(e.target.value)) {
+            setInvalidAddress(false);
+            changeHandler(e);
+        }
+        else {
+            setInvalidAddress(true);
+        }
+    }
+
+    const validatePhone = (e) => {
+        if (/^\d{10}$/.test(e.target.value)) {
+            setInvalidPhone(false);
+            changeHandler(e);
+        }
+        else {
+            setInvalidPhone(true);
+        }
+    }
+
+    const changeHandler = (e) => {
+        const { name, value } = e.target;
+        setEditUserData({
+            ...editUserData,
+            [name]: value
+        })
+
+        console.log(editUserData);
+    }
+
+    const submitEditedData = async (event) => {
+        event.preventDefault();
+        if (invalidFirstName || invalidLastName || invalidAddress || invalidPhone) {
+            alert("invalid data filled!!!");
+            window.location.reload();
+        } 
+        else {
+            await axiosConfig.put(`/user/update/${editData.id}`, editUserData)
+                .then(response => {
+                    alert("Profile Data Edited");
+                    console.log(response.data);
+                    // navigate("/login");
+                })
+                .catch(error => {
+                    alert("provide valid details!!!");
+                })
+        }
+
+    }
+    
+    return (
+        <>
+            <Navbar2 homePath="/customer/home"/>
+            <div className="container mt-5 p-3 border registerdiv" style={{ width: "60%" }}>
+                <div className="" style={{ position: "relative", height: "5rem" }}>
+                    <div className="largeFont zIndBack registerHeadOut" style={{ left: "20%" }}>
+                        Profile
+                    </div>
+                    <div className="smallFont zIndFront registerHeadIn" style={{}}>
+                        Profile
+                    </div>
+                </div>
+
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="firstname" className="form-label">First Name</label>
+                        <input type="text" name="firstName" className="form-control" id="firstname" placeholder={editData.firstName} aria-describedby="" required={true} onChange={(e) => e ? validateFirstName(e) : ""} />
+                        <div>
+                            {invalidFirstName && <><div>Invalid First Name!!!</div></>}
+                        </div>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="lastname" className="form-label">Last Name</label>
+                        <input type="text" name="lastName" className="form-control" id="lastname" placeholder={editData.lastName} aria-describedby="" onChange={(e) => e ? validateLastName(e) : ""} />
+                        <div>
+                            {invalidLastName && <><div>Invalid Last Name!!!</div></>}
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email address</label>
+                        <input type="email" readOnly value={editData.email} name="email" className="form-control" id="email" aria-describedby="emailHelp" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="address" className="form-label">address</label>
+                        <textarea className="form-control" id="address" rows="3" name="address" placeholder={editData.address} onChange={(e) => e ? validateAddress(e) : ""}></textarea>
+                        <div>
+                            {invalidAddress && <><div>Minimum 5 charaters required!!!</div></>}
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="mobile" className="form-label">Mobile no</label>
+                        <input name="phone" type="number" className="form-control" id="mobile" placeholder={editData.phone} aria-describedby="" required={true} onChange={(e) => e ? validatePhone(e) : ""}
+                        />
+                        <div>
+                            {invalidPhone && <><div>Only 10 digits are allowed for mobile Number!</div></>}
+                        </div>
+                    </div>
+                    <Button classname="btn btn-md ps-3 pe-3 mt-1 me-5 customBtn text-light" btnText="Done" clickType="button" onClick={submitEditedData} />
+                </form>
+            </div>
+            <Footer homePath="/"/>
+
+        </>
+    )
+}
+
+export default EditProfile
--- frontend/messfrontend/src/components/header/Navbar2.js
+++ frontend/messfrontend/src/components/header/Navbar2.js
@@ -21,21 +21,21 @@ const Navbar2 = ({ userData, onLogout, homePath }) => {
         <div className="nav_bar  d-flex p-lg-3 col-lg-8 col-md-8 col-12 justify-content-center">
           <div>
             <Link to={homePath}>
-              <Button classname="navbtn btn btn-md text-secondary" btnText="HOME" />
+              <Button classname="navbtn btn btn-md" btnText="HOME" />
             </Link>
 
             <Link to="/about">
-              <Button classname="navbtn btn btn-md text-secondary" btnText="ABOUT US" />
+              <Button classname="navbtn btn btn-md" btnText="ABOUT US" />
             </Link>
 
             <Link to="/contact">
-              <Button classname="navbtn btn btn-md text-secondary" btnText="CONTACT" />
+              <Button classname="navbtn btn btn-md" btnText="CONTACT" />
             </Link>
 
             
               <Link to="/">
                 <Button
-                  classname="navbtn btn btn-md text-secondary"
+                  classname="navbtn btn btn-md"
                   btnText="LOGOUT"
                   onClick={onLogout}
                 />
@@ -44,7 +44,9 @@ const Navbar2 = ({ userData, onLogout, homePath }) => {
               <div className="user-icon-dropdown">
                 <i className="fa-solid fa-user"></i>
                 <div className="dropdown-content">
+                <Link to="/customer/edit-profile">
                   <div className="dropdown-item">Edit Profile</div>
+                </Link>
                   <div className="dropdown-item">Change Password</div>
                 
               </div>
